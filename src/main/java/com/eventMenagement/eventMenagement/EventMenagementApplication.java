d6bc6e725eca4e5301a51fa398017884c71de0d8
Mairon <maironteodoro@gmail.com>
2025-09-16 11:55:12 -0300

    project base

---
+++ src/main/java/com/eventMenagement/eventMenagement/EventMenagementApplication.java
@@ -0,0 +1,13 @@
+package com.eventMenagement.eventMenagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMenagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMenagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventMenagement/eventMenagement/EventMenagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventMenagement.eventMenagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMenagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
