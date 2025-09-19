46428bab5b292994dc280d026c8c78d2e704e122
Mairon <maironteodoro@gmail.com>
2025-09-19 08:49:31 -0300

    add Category entity + CategoryEnum

--- src/main/java/com/eventManagement/eventManagement/entity/Category.java
+++ src/main/java/com/eventManagement/eventManagement/entity/Category.java
@@ -1,7 +1,53 @@
 package com.eventManagement.eventManagement.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
-public class Category {
+public class Category implements Serializable {
+
+    @Serial
+    private static final Long serialVersionUID = 3L;
+
+    public Category(){};
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
