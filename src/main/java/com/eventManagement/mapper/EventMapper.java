cb438f8fb86da389ab7f652b97a90484b43b822a
Mairon <maironteodoro@gmail.com>
2025-09-24 13:53:35 -0300

    add FeedBackService

--- src/main/java/com/eventManagement/mapper/EventMapper.java
+++ src/main/java/com/eventManagement/mapper/EventMapper.java
@@ -1,6 +1,27 @@
-package com.eventManagement.eventManagement;
+package com.eventManagement.mapper;
 
 
+import com.eventManagement.eventManagement.dto.eventDto.CreateEventRequest;
+import com.eventManagement.eventManagement.dto.eventDto.EventResponse;
+import com.eventManagement.eventManagement.dto.eventDto.UpdateEventRequest;
+import com.eventManagement.eventManagement.dto.userDto.UpdateUserRequest;
+import com.eventManagement.eventManagement.entity.Event;
+import com.eventManagement.eventManagement.entity.User;
+import com.eventManagement.eventManagement.service.EventService;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
 
+@Mapper(componentModel = "spring")
 public interface EventMapper {
+
+    FeedBackMapper.EventMapper INSTANCE = Mappers.getMapper(FeedBackMapper.EventMapper.class);
+
+    EventResponse toResponse(Event event);
+
+
+    Event toEntity(CreateEventRequest eventRequest);
+
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void update(UpdateEventRequest userRequest, @MappingTarget Event event);
 }
